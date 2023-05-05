1e3a2829254a70b8e1a6baf3e4dfca6c060f1ce8
adorabled4 <dhx2648466390@163.com>
2023-05-05 17:38:17 +0800

    feat: 页面置换算法算法:LRU Java语言实现

---
+++ 页面置换算法/Java/com/dhx/algorithms/lru/DoubleLinkedList.java
@@ -0,0 +1,68 @@
+package com.dhx.algorithms.lru;
+
+/**
+ * @author dhx_
+ * @className DoubleList
+ * @date : 2023/02/17/ 19:28
+ **/
+public class DoubleLinkedList {
+    /**
+     * 头尾虚节点
+     */
+    private Element head, tail;
+
+    /**
+     * 链表元素数
+     */
+    private int size;
+
+    public DoubleLinkedList() {
+        // 初始化双向链表的数据
+        head = new Element(0, 0);
+        tail = new Element(0, 0);
+        head.next = tail;
+        tail.pre = head;
+        size = 0;
+    }
+
+    /**
+     * 在链表尾部添加节点 x，时间 O(1)
+     * @param x 需要添加的element
+     */
+    public void addLast(Element x) {
+        x.pre = tail.pre;
+        x.next = tail;
+        tail.pre.next = x;
+        tail.pre = x;
+        size++;
+    }
+
+    /**
+     * 删除链表中的 x 节点（x 一定存在） , 由于是双链表且给的是目标 element 节点，时间 O(1)
+     * @param x
+     */
+    public void remove(Element x) {
+        x.pre.next = x.next;
+        x.next.pre = x.pre;
+        size--;
+    }
+
+    /**
+     * 删除链表中第一个节点，并返回该节点，时间 O(1)
+     * @return
+     */
+    public Element removeFirst() {
+        if (head.next == tail)
+            return null;
+        Element first = head.next;
+        remove(first);
+        return first;
+    }
+
+    /**
+     * 返回链表长度，时间 O(1)
+     * @return
+     */
+    public int size() { return size; }
+
+}
---
+++ 页面置换算法/Java/com/dhx/sample/LRUExample.java
@@ -0,0 +1,55 @@
+package com.dhx.sample;
+
+import com.dhx.algorithms.lru.Element;
+import com.dhx.algorithms.lru.LRU;
+
+/**
+ * @author dhx_
+ * @className LruSample
+ * @date : 2023/02/17/ 12:41
+ **/
+public class LRUExample {
+
+    public static void main(String[] args) {
+        System.out.println("测试前准备测试页如下: ");
+        for (Element element : LRU.elements) {
+            System.out.println(element);
+        }
+        // 理论上LRU算法的缺页率会随着 物理块数量的增加而降低 , 如果不明显, 可以通过加大第二个参数(访问次数)尝试
+        testNBlock(3,100);
+        testNBlock(4,100);
+        testNBlock(6,100);
+        testNBlock(8,100);
+        testNBlock(10,100);
+        /*
+        实际测试 参数
+        3 100 [测试结束] 缺页次数: 78缺页率: 0.78 当前页面情况: Pages:{ Element {key=2, val=10002}--> Element {key=4, val=10004}--> Element {key=9, val=10009} }
+        4 100 [测试结束] 缺页次数: 65缺页率: 0.65 当前页面情况: Pages:{ Element {key=1, val=10001}--> Element {key=2, val=10002}--> Element {key=4, val=10004}--> Element {key=7, val=10007} }
+        6 100 [测试结束] 缺页次数: 53缺页率: 0.53 当前页面情况: Pages:{ Element {key=1, val=10001}--> Element {key=3, val=10003}--> Element {key=4, val=10004}--> Element {key=6, val=10006}--> Element {key=8, val=10008}--> Element {key=10, val=10010} }
+        8 100 [测试结束] 缺页次数: 34缺页率: 0.34 当前页面情况: Pages:{ Element {key=1, val=10001}--> Element {key=2, val=10002}--> Element {key=3, val=10003}--> Element {key=4, val=10004}--> Element {key=6, val=10006}--> Element {key=7, val=10007}--> Element {key=8, val=10008}--> Element {key=10, val=10010} }
+        10 100 [测试结束] 缺页次数: 0缺页率: 0.0 当前页面情况: Pages:{ Element {key=1, val=10001}--> Element {key=2, val=10002}--> Element {key=3, val=10003}--> Element {key=4, val=10004}--> Element {key=5, val=10005}--> Element {key=6, val=10006}--> Element {key=7, val=10007}--> Element {key=8, val=10008}--> Element {key=9, val=10009}--> Element {key=10, val=10010} }
+         */
+    }
+
+    /**
+     * 随机调用十次页数
+     * @param n 设置物理块数量为n
+     * @param times 测试次数
+     */
+    static void testNBlock(int n,int times){
+        System.out.println("\u001B[31m\u001B[1m[测试开始]\u001B[0m 本次测试物理块数量为 "+n);
+        if(n>10){
+            throw new RuntimeException("测试块数过多!");
+        }
+        // 设置lru序列的容量为n
+        LRU cache = new LRU(n);
+        // 假设我们最开始直接放入3个页面到内存块中，
+        for (int i = 0; i < n; i++) {
+            cache.put(LRU.elements[i]);
+        }
+        for (int i = 1; i <= times; i++) {
+            cache.get((int)(Math.random()*100)%10+1);
+        }
+        System.out.println("\u001B[31m\u001B[1m[测试结束]\u001B[0m 缺页次数: "+cache.getMissingNum() + "缺页率: "+cache.getMissingNum()/(double)times + " 当前页面情况: " + cache.toString() );
+    }
+}
