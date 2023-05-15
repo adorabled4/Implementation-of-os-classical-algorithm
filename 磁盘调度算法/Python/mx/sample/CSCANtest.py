d9af55e21a185430ddb7899968142f41670f7fc2
Haoxin Dong <dhx2648466390@163.com>
2023-05-15 11:55:10 +0800

    Revert "ma"

--- 磁盘调度算法/Python/mx/sample/CSCANtest.py
+++
@@ -1,4 +0,0 @@
-from CSCAN import cscan
-start=100 #开始的磁道
-distdata=[150,160,184,90,58,55,39,38,18]
-cscan(start,distdata)
--- 磁盘调度算法/Python/mx/sample/FCFStest.py
+++
@@ -1,4 +0,0 @@
-from FCFS import fcfs
-stat=53 #开始的磁道
-distdata=[98,183,37,122,14,124,65,67]
-fcfs(stat,distdata)
